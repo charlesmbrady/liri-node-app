7e8b72c30a02f8d9fe091390b45f15ba6686b694
Charles <charlesmbrady@gmail.com>
2019-02-15 19:39:40 -0500

    got API calls for song and movie request working and formatted

--- liri.js
+++ liri.js
@@ -1,26 +1,145 @@
+/**** Dependencies ******/
+
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
 var fs = require("fs");
-var commands = require("./commands");
+
+/********************** */
+var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
-var input = process.argv[3];
+/////////////////handle multiword input/////////////
+var arguments = process.argv;
+var inputWords = [];
+var input = "";
+
+for (var i = 3; i < arguments.length; i++) {
+    inputWords.push(arguments[i]);
+}
+input = inputWords.join(" ");
+
+/*________________________________________________________*/
+
 
 switch (command) {
     case "concert-this":
-        commands.concertThis(input);
+        concertThis(input);
         break;
     case "spotify-this-song":
-        commands.spotifyThisSong(input);
+        spotifyThisSong(input);
         break;
     case "movie-this":
-        commands.movieThis(input);
+        movieThis(input);
         break;
     case "do-what-it-says":
-        commands.doWhatItSays();
+        doWhatItSays();
         break;
     default:
         console.log("Please enter a valid command...");
         break;
 }
 
+
+
+//__________________________    Functions   ____________________________//
+
+function concertThis(artist) {
+    console.log("You searched for artist: " + artist);
+    
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(function (response) {
+        var events = response.data;
+
+        events.forEach(function (event) {
+            console.log("****************************************************************************************");
+            console.log("You searched for artist: " + artist);
+            console.log("________________________________________");
+            console.log("Venue: " + event.venue.name);
+            console.log("________________________________________");
+            console.log("Venue Location: " + event.venue.city);
+            console.log("________________________________________");
+            //TODO: reformat datetime as "MM/DD/YYYY" with Moment.js
+            console.log("Date: " + event.datetime);
+            console.log("****************************************************************************************");
+        });
+    });
+}
+
+function movieThis(movie) {
+
+    //return default movie data for Mr. Nobody if no movie is declared
+    if (!movie) {
+        movie = "Mr. Nobody";
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(function (response) {
+        console.log("****************************************************************************************");
+        console.log("You searched for movie: " + movie);
+        console.log("________________________________________");
+        console.log("Title: " + response.data.Title);
+        console.log("________________________________________");
+        console.log("Release Year: " + response.data.Year);
+        console.log("________________________________________");
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("________________________________________");
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("________________________________________");
+        console.log("Country where produced: " + response.data.Country);
+        console.log("________________________________________");
+        console.log("Language: " + response.data.Language);
+        console.log("________________________________________");
+        console.log("Plot: " + response.data.Plot);
+        console.log("________________________________________");
+        console.log("Actors: " + response.data.Actors);
+        console.log("****************************************************************************************");
+
+    }).catch(function (err) {
+        console.log("There was an error: " + err);
+    });//TODO: why does this not work?
+
+}
+
+function spotifyThisSong(song) {
+    console.log("________________________________________");
+    console.log("You searched for song: " + song);
+    console.log("________________________________________");
+
+    spotify.search({ type: 'track', query: song, limit: 2 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var results = data.tracks.items;
+        //console.log(data.tracks); //log that song search result
+        results.forEach(function (track, i) {
+            console.log("Track " + (i + 1));
+            var artists = track.artists;
+
+            console.log("Artist(s):");
+            artists.forEach(function (artist) {
+                console.log("--" + artist.name);
+            });
+
+            var trackName = track.name
+            console.log("Track name: " + trackName);
+
+            var trackAlbum = track.album.name;
+            console.log("Album: " + trackAlbum);
+            console.log("__________________________________");
+
+            var previewURL = track.preview_url;
+            if (previewURL == null) {
+                previewURL = "No preview available";
+            }
+            console.log("Preview URL: " + previewURL);
+            console.log("__________________________________");
+        });
+    });
+}
+
+function doWhatItSays() {
+    console.log("You said to do what it says...");
+    //TODO: make this work
+}
