0a9038a4b99572538965d629c84b4fcb5bc47468
Charles <charlesmbrady@gmail.com>
2019-02-17 14:06:41 -0500

    commands and outputs are now logged to the file log.txt

--- liri.js
+++ liri.js
@@ -45,6 +45,12 @@ switch (command) {
 //__________________________    Functions   ____________________________//
 //TODO:limiting API responses
 function concertThis(artist) {
+    //first log the command to the log.txt file
+    fs.appendFile("log.txt", "**********************************************************" + "\r\n\n" + process.argv.slice(2).join(" ") + ", "  + '\r\n\n', function(err){
+        if(err){
+            console.log("there was an error");
+        }
+    });
     console.log("You searched for artist: " + artist);
 
     /*_____________________ Bands In Town API call___________________*/
@@ -55,7 +61,7 @@ function concertThis(artist) {
     //loop through each event in the response and log the Artist Name, Venue Name, and Venue City
         events.forEach(function (event) {
             console.log("****************************************************************************************");
-            console.log("You searched for artist: " + artist);
+            console.log("Artist: " + artist);
             console.log("________________________________________");
             console.log("Venue: " + event.venue.name);
             console.log("________________________________________");
@@ -65,11 +71,24 @@ function concertThis(artist) {
             var convertedDate = moment(event.datetime, "YYYY-MM-DDTHH:mm:ss")
             console.log("Date: " + convertedDate.format("MMM do hh:mm a"));
             console.log("****************************************************************************************");
+
+            //log each result to log.txt
+            fs.appendFile("log.txt", "Artist: " + artist + '\r\n\n' + "Venue: " + event.venue.name + '\r\n\n' + "Venue Location: " + event.venue.city + '\r\n\n' + "Date: " + convertedDate.format("MMM do hh:mm a") + '\r\n\n' + "_____________________________________________________________________________" + '\r\n\n', function(err){
+                if(err){
+                    console.log("there was an error");
+                }
+            });
         });
     });
 }
 
 function movieThis(movie) {
+    //first log the command to the log.txt file
+    fs.appendFile("log.txt", "****************************************************************************************" + '\r\n\n' + process.argv.slice(2).join(" ") + ", " + '\r\n\n', function(err){
+        if(err){
+            console.log("there was an error");
+        }
+    });
 
     //return default movie data for Mr. Nobody if no movie is declared
     if (!movie) {
@@ -99,6 +118,12 @@ function movieThis(movie) {
         console.log("Actors: " + response.data.Actors);
         console.log("****************************************************************************************");
 
+        //log the search result to log.txt
+        fs.appendFile("log.txt", "You searched for movie: " + movie + "\r\n" + "Title: " + response.data.Title + "\r\n" + "Release Year: " + response.data.Year + "\r\n\n" + "IMDB Rating: " + response.data.imdbRating + "\r\n\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n\n" + "Country where produced: " + response.data.Country + "\r\n\n" + "Language: " + response.data.Language + "\r\n\n" + "Plot: " + response.data.Plot + "\r\n\n" + "Actors: " + response.data.Actors + "\r\n\n", function(err){
+            if(err){
+                console.log("there was an error");
+            }
+        });
     }).catch(function (err) {
         console.log("There was an error: " + err);
     });//TODO: why does this not work?
@@ -106,6 +131,12 @@ function movieThis(movie) {
 }
 
 function spotifyThisSong(song) {
+    //first log the command to the log.txt file
+    fs.appendFile("log.txt", "****************************************************************************************" + '\r\n\n' + process.argv.slice(2).join(" ") + ", "  + '\r\n\n', function(err){
+        if(err){
+            console.log("there was an error");
+        }
+    });
 
     /*if(!song){
         spotify.request("https://api.spotify.com/v1/search/q=the+sign%20artist:ace+of+base").then(function (response) {
@@ -130,11 +161,14 @@ function spotifyThisSong(song) {
             console.log("Track " + (i + 1));
             var artists = track.artists;
             console.log("\r\n");
+            var temp = [];
 
             console.log("Artist(s):");
             artists.forEach(function (artist) {
                 console.log("--" + artist.name);
+                temp.push(artist.name);
             });
+            temp.join(", ");
             console.log("\r\n");
 
             var trackName = track.name
@@ -151,11 +185,25 @@ function spotifyThisSong(song) {
             }
             console.log("Preview URL: " + previewURL);
             console.log("__________________________________");
+
+            //append results to log.txt
+            fs.appendFile("log.txt", "Track " + (i + 1) + "\r\n\n" + "Artist(s): " + temp + "\r\n\n" + "Track name: " + trackName + "\r\n\n" + "Album: " + trackAlbum + "\r\n\n" + "Preview URL: " + previewURL + "\r\n\n" + "_______________________________________________________" + "\r\n\n" , function(err){
+                if(err){
+                    console.log("there was an error");
+                }
+            });
+            
         });
     });
 }
 
 function doWhatItSays() {
+    //first log the command to the log.txt file
+    /*fs.appendFile("log.txt", "****************************************************************************************" + '\r\n\n' + process.argv.slice(2).join(" ") + ", "  + '\r\n\n', function(err){
+        if(err){
+            console.log("there was an error");
+        }
+    });*/
     console.log("You said to do what it says...");
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
@@ -186,7 +234,3 @@ function doWhatItSays() {
         }
     });
 }
-
-/*TODO:     In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
-Make sure you append each command you run to the log.txt file. 
-Do not overwrite your file each time you run a command. */
